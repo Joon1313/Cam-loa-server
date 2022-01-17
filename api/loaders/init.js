ec2a15a725b282acde39e6da426cd00e0c1384ff
Joon1313 <wkdrud13@gmail>
2022-01-17 12:44:29 +0900

    gitignore

--- api/loaders/init.js
+++ api/loaders/init.js
@@ -5,8 +5,11 @@ const greenlockInit = require("./greenlock");
 async function loaderInit(app) {
   try {
     await expressInit(app);
+    console.log("Express Init");
     await mongooseInit();
+    console.log("MongoDB Connected");
     await greenlockInit(app);
+    console.log("GreenLock Start");
   } catch (err) {
     console.log(err);
   }
