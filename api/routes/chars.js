33eb612ac87d0345cf2a234fd837a895db286ebc
Joon1313 <wkdrud13@gmail.com>
2022-01-17 13:07:08 +0900

    char service

--- api/routes/chars.js
+++ api/routes/chars.js
@@ -1,35 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const cheerio = require("cheerio");
-const axios = require("axios");
+const CharService = require("../../services/char");
 
 router.get("/:name", async (req, res, next) => {
+  if (!req.params.name) res.status(400).json({ error: "not found name" });
   try {
-    let userLevel;
-    let userJob;
-    await axios
-      .get(
-        `https://lostark.game.onstove.com/Profile/Character/${encodeURI(
-          req.params.name
-        )}`
-      )
-      .then((r) => {
-        const $ = cheerio.load(r.data);
-        userLevel = $(
-          "#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info2 > div.level-info2__item > span:nth-child(2)"
-        ).text();
-        userJob = $(
-          "#lostark-wrapper > div > main > div > div.profile-character-info > img"
-        ).attr("alt");
-        userLevel = userLevel.substring(3);
-        userLevel = userLevel.substring(0, userLevel.length - 3);
-      })
-      .catch((Error) => {
-        console.log(Error);
-      });
-    res.json({ level: userLevel, job: userJob });
-  } catch (e) {
-    res.status(500).send(e);
+    const data = await CharService.search(req.params.name);
+    res.json(data);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
   }
 });
 
--- services/char.js
+++ services/char.js
@@ -1,6 +1,37 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-class CharService {}
+class CharService {
+  async search(name) {
+    const res = await axios.get(
+      `https://lostark.game.onstove.com/Profile/Character/${encodeURI(name)}`
+    );
+    const level = getLevel(res.data);
+    const job = getJob(res.data);
+    const result = {
+      level,
+      job,
+    };
+    return result;
+  }
+}
 
+function getLevel(data) {
+  let level;
+  const $ = cheerio.load(data);
+  level = $(
+    "#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info2 > div.level-info2__item > span:nth-child(2)"
+  ).text();
+  level = level.substring(3);
+  level = level.substring(0, level.length - 3);
+  return level;
+}
+
+function getJob(data) {
+  const $ = cheerio.load(data);
+  const job = $(
+    "#lostark-wrapper > div > main > div > div.profile-character-info > img"
+  ).attr("alt");
+  return job;
+}
 module.exports = new CharService();
