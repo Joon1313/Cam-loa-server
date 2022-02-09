fb448dfc8673b567664585edb212c44148c3dd05
Joon1313 <wkdrud13@gmail.com>
2022-02-09 09:11:36 +0900

    임시코드 삭제 및 점검시간 요청 제한

--- api/routes/chars.js
+++ api/routes/chars.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const CharService = require("../../services/char");
 
 router.get("/:name", async (req, res, next) => {
-  if (!req.params.name)
-    return res.status(400).json({ error: "not found name" });
+  const 현재시간 = new Date();
+  if (현재시간.getDay() === 3 && 5 < 현재시간.getHours() < 11)
+    return res.status(400).json({ error: "공식 홈페이지 점검시간 입니다." });
+  if (!req.params.name) return res.status(400).json({ error: "캐릭터명이 존재하지 않습니다" });
+
   try {
     const data = await CharService.search(req.params.name);
     res.json(data);
--- api/routes/comments.js
+++ api/routes/comments.js
@@ -2,20 +2,6 @@ const express = require("express");
 const router = express.Router();
 const CommentService = require("../../services/comment");
 
-// router.use(function timeLog(req, res, next) {
-//   console.log("Time: ", Date.now());
-//   next();
-// });
-// 임시 코드 !! 3일뒤 삭제! 무한스크롤 구현으로 더이상 사용안함
-router.get("/", async (req, res) => {
-  try {
-    const comment = await CommentService.find();
-    res.json(comment);
-  } catch (err) {
-    res.status(500).send({ error: err.message });
-  }
-});
-
 router.get("/:page", async (req, res) => {
   try {
     const page = Number(req.params.page);
@@ -27,7 +13,7 @@ router.get("/:page", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  if (!req.body.name || !req.body.content) return res.status(400).send({ error: "name or content is null" });
+  if (!req.body.name || !req.body.content) return res.status(400).send({ error: "제목, 내용 은 필수데이터 입니다." });
   try {
     const comment = await CommentService.create(req);
     res.json(comment);
